bc757f163544454a5e45b921f1170f5f99ad8b95
AndreasH96 <andreashaggstrom96@gmail.com>
2024-02-23 16:13:01 +0100

    basic functionality kinda works

--- src/main.rs
+++ src/main.rs
@@ -35,42 +35,54 @@ fn generate_coordinate_for_distance(origin: WGS84<f32>, distance: i32) -> WGS84<
     new_pos
 }
 
-fn main() {
-    let start: WGS84<f32> = WGS84::from_degrees_and_meters(40.6976312,-74.1444842, 0.0);
 
-    let text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
-
-    let text_hex = hex::encode(text);
+fn encode(origin:WGS84<f32>,text:&str) -> Vec<WGS84<f32>>{
     let window_size = 4;
     let mut coordinates: Vec<WGS84<f32>> = vec![];
     println!("Encoding text {:?}", text);
 
-    for i in (0..(text_hex.len())).step_by(window_size) {
-        let window_size = std::cmp::min(window_size, text_hex.len() - i); // Adjust size for last window
-        let window = text_hex
+    for i in (0..(text.len())).step_by(window_size) {
+        let window_size = std::cmp::min(window_size, text.len() - i); // Adjust size for last window
+        let window = text
             .chars()
             .skip(i)
             .take(window_size)
             .collect::<String>();
 
         coordinates.push(generate_coordinate_for_distance(
-            start,i32::from_str_radix(&window, 16).unwrap()
+            origin,i32::from_str_radix(&window, 16).unwrap()
             ,
         ))
     }
+    coordinates
+}
 
+fn decode(origin: WGS84<f32>,encoded:Vec<WGS84<f32>>) -> String{
+    String::from_utf8(
+        hex::decode(
+            encoded
+                    .iter()
+                    .map(|x| format!("{:x}", origin.distance(x).floor() as i32))
+                    .join(""),
+            )
+            .unwrap(),
+        )
+        .unwrap_or(String::from("Failed to parse, incorrect coordinates!"))
+}
+
+
+
+fn main() {
+    let start: WGS84<f32> = WGS84::from_degrees_and_meters(40.6976312,-74.1444842, 0.0);
+
+    let text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+
+    let text_hex = hex::encode(text);
+
+    let encoded = encode(start,&text_hex);
 
     println!("\nRestoring to text");
-    let decoded = String::from_utf8(
-    hex::decode(
-            coordinates
-                .iter()
-                .map(|x| format!("{:x}", start.distance(x).floor() as i32))
-                .join(""),
-        )
-        .unwrap(),
-    )
-    .unwrap_or(String::from("Failed to parse, incorrect coordinates!"));
+    let decoded = decode(start,encoded);
 
     println!("Restored text:{:?}", decoded);
 
